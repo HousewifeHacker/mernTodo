8f068864b64f304694b9a312170c216314944cfc
housewifehacker <housewifehacker@gmail.com>
2019-02-21 08:43:51 -0600

    Add proptype to check Task component

--- client/src/components/task.js
+++ client/src/components/task.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import {
     Button,
     ListGroupItem,
@@ -8,7 +9,7 @@ import { Draggable } from 'react-beautiful-dnd';
 export default class Task extends Component {
     render() {
         const {_id, name, index, removeTask} = this.props;
-        
+
         return (
             <Draggable draggableId={_id} index={index}>
                 {(provided, snapshot) => ( 
@@ -35,3 +36,10 @@ export default class Task extends Component {
         );
     }
 }
+
+Task.propTypes = {
+    _id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    index: PropTypes.number.isRequired,
+    removeTask: PropTypes.func.isRequired
+};

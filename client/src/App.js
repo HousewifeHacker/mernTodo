5a3020ad13661bf5d890f121c5e8e484d4987846
housewifehacker <housewifehacker@gmail.com>
2019-06-30 13:42:54 -0500

    Handle dependency version incompatibilities with upgrades. Include fontawesome. Not used yet but it will be.

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Switch, Link } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';

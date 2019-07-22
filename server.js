c8416ecda36f5fc44f04de9bc48826fe98d0ea53
housewifehacker <housewifehacker@gmail.com>
2019-07-21 23:09:06 -0500

    try getting static from react build in production mode

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 //internal
 const users = require('./routes/api/users');
@@ -24,5 +25,13 @@ app.use('/api/sessions', sessions);
 app.use('/api/tasks', tasks);
 app.use('/api/lists', lists);
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log('found me'));
+app.listen(port, () => console.log('found api'));
